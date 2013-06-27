f45f44399e3385b55b6ff30409e98b397f85592f
JohanLarsson <johan_gavle1@hotmail.com>
2013-06-27 17:02:44 +0200

    Initial

---
+++ CommandDemo/DummyVm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CommandDemo
+{
+    public class DummyVm
+    {
+        public DummyVm()
+        {
+            DummyCommand= new DelegateCommand()
+        }
+        public ICommand DummyCommand { get; private set; }
+    }
+}
